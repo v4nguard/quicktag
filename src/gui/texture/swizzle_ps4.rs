268e52b4782bdee7ec5eb51faf38d9283d82006a
cohaereo <cohae@cohae.dev>
2025-01-13 19:57:48 +0100

    texture: Add slice swizzling

--- src/gui/texture/swizzle_ps4.rs
+++ src/gui/texture/swizzle_ps4.rs
@@ -36,7 +36,7 @@ fn do_swizzle(
     destination: &mut [u8],
     width: usize,
     height: usize,
-    _depth_or_array_size: usize,
+    depth_or_array_size: usize,
     format: GcnSurfaceFormat,
     unswizzle: bool,
     align_output: bool,
@@ -59,31 +59,37 @@ fn do_swizzle(
     let height_texels_aligned = (height_texels + 7) / 8;
     let mut data_index = 0;
 
-    for y in 0..height_texels_aligned {
-        for x in 0..width_texels_aligned {
-            for t in 0..64 {
-                let pixel_index = morton(t, 8, 8);
-                let div = pixel_index / 8;
-                let rem = pixel_index % 8;
-                let x_offset = (x * 8) + rem;
-                let y_offset = (y * 8) + div;
+    for z in 0..depth_or_array_size {
+        let slice_dest = &mut destination[(z * width * height * format.bpp()) / 8..];
 
-                if x_offset < width_texels_dest && y_offset < height_texels_dest {
-                    let dest_pixel_index = y_offset * width_texels_dest + x_offset;
-                    let dest_index = block_size * dest_pixel_index;
-                    let (src, dst) = if unswizzle {
-                        (data_index, dest_index)
-                    } else {
-                        (dest_index, data_index)
-                    };
+        for y in 0..height_texels_aligned {
+            for x in 0..width_texels_aligned {
+                for t in 0..64 {
+                    let pixel_index = morton(t, 8, 8);
+                    let div = pixel_index / 8;
+                    let rem = pixel_index % 8;
+                    let x_offset = (x * 8) + rem;
+                    let y_offset = (y * 8) + div;
 
-                    if (src + block_size) < source.len() && (dst + block_size) < destination.len() {
-                        destination[dst..dst + block_size]
-                            .copy_from_slice(&source[src..src + block_size]);
+                    if x_offset < width_texels_dest && y_offset < height_texels_dest {
+                        let dest_pixel_index = y_offset * width_texels_dest + x_offset;
+                        let dest_index = block_size * dest_pixel_index;
+                        let (src, dst) = if unswizzle {
+                            (data_index, dest_index)
+                        } else {
+                            (dest_index, data_index)
+                        };
+
+                        if (src + block_size) < source.len()
+                            && (dst + block_size) < slice_dest.len()
+                        {
+                            slice_dest[dst..dst + block_size]
+                                .copy_from_slice(&source[src..src + block_size]);
+                        }
                     }
-                }
 
-                data_index += block_size;
+                    data_index += block_size;
+                }
             }
         }
     }
